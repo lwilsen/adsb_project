b4e82c83593f4054f9d3d4b174e97fcae119eb7b
lwilsen <lwilsen@umass.edu>
2024-09-09 14:27:00 -0400

    Fixing pylint errors and added a requirements.txt for the pylint setup

--- kpler_test.py
+++ kpler_test.py
@@ -1,15 +1,18 @@
+"""Importing these modules to get the environment variable KPLER token, 
+and to use the requests module to access the KPLER api."""
+
 import os
 import requests
 
 kpler_access_token = os.getenv("KPLER_TOKEN")
 
-url = "https://api-lng.kpler.com/v1/trades?size=5"
+URL = "https://api-lng.kpler.com/v1/trades?size=5"
 
 payload = {}
 headers = {
     'Authorization': f"Basic {kpler_access_token}"
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", URL, headers=headers, data=payload)
 
 print(response.text)
