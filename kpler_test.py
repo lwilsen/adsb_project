ff8a92480024848282d7b5d04a08fe3d8f554557
lwilsen <lwilsen@umass.edu>
2024-09-09 14:47:22 -0400

    blacked all files

--- kpler_test.py
+++ kpler_test.py
@@ -10,9 +10,7 @@ kpler_access_token = os.getenv("KPLER_TOKEN")
 URL = "https://api-lng.kpler.com/v1/trades?size=5"
 
 payload = {}
-headers = {
-    'Authorization': f"Basic {kpler_access_token}"
-}
+headers = {"Authorization": f"Basic {kpler_access_token}"}
 
 response = requests.api.get(URL, headers=headers, data=payload, timeout=10)
 
